94a80ecca95a10804860b0578f4d862421837843
RobinLe <web@robinle.de>
2023-05-04 09:42:23 +0200

    Keyboard class implemented

--- include/rlInput/Keyboard.hpp
+++ include/rlInput/Keyboard.hpp
@@ -6,10 +6,121 @@
 
 
 
+// STL
+#include <memory>
+#include <string>
+
+// Win32
+#define WIN32_MEAN_AND_LEAN
+#define NOMINMAX
+#include <Windows.h>
+#undef WIN32_MEAN_AND_LEAN
+#undef NOMINMAX
+
+
+
 namespace rlInput
 {
 
-	// TODO: Keyboard interface
+	class Keyboard
+	{
+	public: // types
+
+		struct Key
+		{
+			bool bPressed;
+			bool bHeld;
+			bool bReleased;
+		};
+
+
+
+
+
+	public: // static methods
+
+		static Keyboard &Instance() { return s_oInstance; }
+
+
+	private: // static variables
+
+		static Keyboard s_oInstance;
+
+
+
+
+
+	public: // methods
+
+		const Key &operator[](unsigned char index) const { return m_upStates[index]; }
+
+		/// <summary>
+		/// Prepare the internal key info for queries.<para />
+		/// Must be called every time an updated state of the keys is required.
+		/// </summary>
+		void prepare();
+
+		/// <summary>
+		/// Try to process a Windows message.<para/>
+		/// Should be called every time a Windows message is received.
+		/// </summary>
+		/// <returns>Was the message keyboard-input related?</returns>
+		bool update(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) noexcept;
+
+		/// <summary>
+		/// Reset the inner state of the keyboard to "no button down".<para />
+		/// Also stops text recording and clears the previously recorded text.
+		/// </summary>
+		void reset() noexcept;
+
+
+
+		/// <summary>
+		/// Start recording text input.<para/>
+		/// Text is recorded only if the window sending messages uses Unicode
+		/// (<c>RegisterClassW</c> was used).
+		/// </summary>
+		void startTextRecording() noexcept { m_bRecordText = true; }
+		/// <summary>
+		/// End recording text input.
+		/// </summary>
+		void endTextRecording() noexcept { m_bRecordText = m_bRecordingStopped = false; }
+
+		/// <summary>
+		/// Is text currently being recorded?
+		/// </summary>
+		bool recordingText() const noexcept { return m_bRecordText; }
+
+		/// <summary>
+		/// The text recorded via <c>startTextRecording()</c> up until now.<para/>
+		/// New text input will always be appended.
+		/// You can call <c>clearRecordedText()</c> to empty this string.
+		/// </summary>
+		auto &recordedText() const noexcept { return m_sRecordedText; }
+
+		/// <summary>
+		/// Clear the text recorded via <c>startTextRecording()</c> up until now.
+		/// </summary>
+		void clearRecordedText() noexcept { m_sRecordedText.clear(); }
+
+
+	private: // methods
+
+		Keyboard(); // --> singleton
+		~Keyboard() = default;
+
+
+	private: // variables
+
+		std::unique_ptr<Key[]> m_upStates = std::make_unique<Key[]>(256);
+		bool m_oRawStates_Old[256]{};
+		bool m_oRawStates_New[256]{};
+
+		bool m_bRecordText       = false;
+		bool m_bRecordingStopped = true;
+		std::wstring m_sRecordedText;
+
+	};
 
 }
 
--- src/Keyboard.cpp
+++ src/Keyboard.cpp
@@ -2,7 +2,87 @@
 
 namespace rlInput
 {
-	
-	// TODO: Keyboard implementation
+
+	Keyboard::Keyboard()
+	{
+		memset(m_upStates.get(), 0, sizeof(Key) * 256);
+	}
+
+	void Keyboard::prepare()
+	{
+		Key        *pDest   = m_upStates.get();
+		const bool *pRawOld = m_oRawStates_Old;
+		const bool *pRawNew = m_oRawStates_New;
+
+		for (unsigned i = 0; i < 256; ++i)
+		{
+			pDest->bPressed  =  *pRawNew && !*pRawOld;
+			pDest->bHeld     =  *pRawNew;
+			pDest->bReleased = !*pRawNew &&  *pRawOld;
+
+			++pDest;
+			++pRawOld;
+			++pRawNew;
+		}
+
+		memcpy_s(
+			m_oRawStates_Old, sizeof(m_oRawStates_Old),
+			m_oRawStates_New, sizeof(m_oRawStates_New)
+		);
+	}
+
+	bool Keyboard::update(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) noexcept
+	{
+		switch (uMsg)
+		{
+
+		case WM_KEYDOWN:
+			m_oRawStates_New[wParam] = true;
+			break;
+
+		case WM_KEYUP:
+			m_oRawStates_New[wParam] = false;
+			break;
+
+		case WM_CHAR:
+			if ((!m_bRecordText && m_bRecordingStopped) || !IsWindowUnicode(hWnd))
+				return false;
+
+			if (!m_bRecordText)
+			{
+				if (!m_sRecordedText.empty() && IS_HIGH_SURROGATE(*m_sRecordedText.end()))
+				{
+					if (IS_LOW_SURROGATE(lParam))
+						m_sRecordedText += (wchar_t)lParam;
+					else
+						m_sRecordedText.pop_back();
+				}
+
+				m_bRecordingStopped = true;
+				return false;
+			}
+
+			if (!IS_LOW_SURROGATE(lParam) ||
+				(m_sRecordedText.length() > 0 && IS_HIGH_SURROGATE(*m_sRecordedText.end())))
+				m_sRecordedText += (wchar_t)lParam;
+
+			return false;
+
+		default:
+			return false;
+		}
+
+		return true;
+	}
+
+	void Keyboard::reset() noexcept
+	{
+		memset(m_oRawStates_Old, 0, sizeof(m_oRawStates_Old));
+		memset(m_oRawStates_New, 0, sizeof(m_oRawStates_New));
+
+		m_bRecordText       = false;
+		m_bRecordingStopped = true;
+		m_sRecordedText.clear();
+	}
 
 }
